c57f4bd6ef5716b026e9d587bd15f6d26b97ab1f
hazzaldo <hazzaldo@hotmail.com>
2019-04-03 22:57:50 +0100

    add NN function

--- simple Neural Network in pure python (no ml packages).py
+++ simple Neural Network in pure python (no ml packages).py
@@ -7,3 +7,9 @@ Created on Wed Apr  3 17:40:03 2019
 
 # ------- Simple Neural Network written in pure python (No ML packages) ----------
 
+import numpy as np
+
+# Create the neural network function
+def NN(m1, m2, w1, w2, b):
+    input_weighted_sum = m1 + m2 * w2 + b
+    return sigmoid(input_weighted_sum)
