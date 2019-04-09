83c390533261d7f8d7ab8848091b26b6fb693ac2
hazzaldo <hazzaldo@hotmail.com>
2019-04-09 13:58:30 +0100

    make logical corrections to model and add printout results of prediction

--- simple Neural Network in pure python (no ml packages).py
+++ simple Neural Network in pure python (no ml packages).py
@@ -6,8 +6,10 @@ Created on Wed Apr  3 17:40:03 2019
 """
 
 # ------- Simple Neural Network written in pure python (No ML packages) ----------
+from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 import os
 
 class NN_classification:
@@ -20,11 +22,16 @@ class NN_classification:
         self.training_weighted_sum = float()
         self.test_weighted_sum = float()
         self.chosen_activation = None
+        self.chosen_cost_func = None
         self.train_activation_func_output = float()
         self.test_activation_func_output = float()
         self.dWeightedSum_dWeights = []
         self.dCost_dWeights = []
         self.costs = []
+        self.train_average_accuracy = int()
+        self.test_average_accuracy = int()
+        self.test_predictions = []
+        self.train_predictions = []
     
     # -- Activation functions --: 
     def sigmoid(x):
@@ -42,6 +49,10 @@ class NN_classification:
             return 0
         else:
             return 1
+    
+    # -- cost function --:
+    def squared_error(pred, target):
+        return np.square(pred - target)
 
     """  
      This is a simple 1 layer neural network function, that solves binary classification problems. It takes the following parameters:
@@ -51,6 +62,7 @@ class NN_classification:
          epochs: (type: integer) number of times you wish for the neural network to perform forward and back propagation updating the weights and bias. The more epochs performed the more accurate / optimised the prediction will be, but the more time and computation it will take.
          activation_func: (optional - default: 'sigmoid') (type: string) specify the activation function to be used. The default is 'sigmoid' if nothing is specified in this parameter. Although 'relu' is proved quicker for a neural network to learn, it's best to use for the hidden layers, while sigmoid is best used for output layer because of the dealing with probability. Since we only have 1 layer (considered the output layer as well) so sigmoid is best to be the default. At the moment the network only accepts 'relu' or 'sigmoid' function. A potential improvement to add more activation functions in future.
         learning_rate: (optional - default: 0.2) (type: decimal/float) the fraction/percentage of the derivative to use to do weight and bias correction in a single epoch. The higher the fraction/percentage the quicker the weights and bias will be corrected per epoch. But a learning rate that is too high may overshoot in finding the minimum point of error (best optimisation). A good range would be between 0.1 - 0.4
+        cost_func: (optional - default: 'squared_error') (type: string) the cost function to use in the neural network to calculate the error margin between prediction and target label output.
     """
     # --- nueral network structure diagram --- 
 
@@ -58,11 +70,12 @@ class NN_classification:
     #   / \   w1, w2, b
     #  O   O  datapoint 1, datapoint 2
 
-    def simple_1_layer_classification_NN(self, dataset_input_matrix, output_data_label, input_dimension, epochs, activation_func='sigmoid', learning_rate=0.2):
+    def simple_1_layer_classification_NN(self, dataset_input_matrix, output_data_labels, input_dimension, epochs, activation_func='sigmoid', learning_rate=0.2, cost_func='squared_error'):
         # The number of weights we will use will depend on number of our input sample data. Each data value will get its own weight.
-        self.num_of_weights = (len(input_dimension))
+        self.num_of_weights = input_dimension
         self.dataset_input_matrix = dataset_input_matrix
-        
+        correct_pred = int()
+        incorrect_pred = int()
         # Set initial network parameters (weights & bias):
         # Will initialise the weights to a uniform distribution and ensure the numbers are small close to 0.
         # We need to loop through all the weights to set them to a random value initially.
@@ -92,23 +105,43 @@ class NN_classification:
             self.chosen_activation = activation_func
             if activation_func == 'sigmoid':
                 self.train_activation_func_output = NN_classification.sigmoid(self.training_weighted_sum)
-            else:
+            elif activation_func == 'relu':
                 self.train_activation_func_output = NN_classification.relu(self.training_weighted_sum)
-        
+            else:
+                print("Exception error - no activation function utilised, in training method", file=sys.stderr)
+                return
+                
             # Prediction: Because this is a single layer neural network, so the activation will be the same as the prediction
             pred = self.train_activation_func_output
+            self.train_predictions.append(pred)
             
-            target = output_data_label
+            # store the chosen activation function to refer to in the 'predict' function.
+            self.chosen_cost_func = cost_func
             
-            # Cost: we use the error squared function as the cost function to calculate the prediction error margin
-            cost = np.square(pred - target)
+            # The variable to store the cost value
+            cost = None
+            
+            # The variable to store the derivative of the cost function with respect to prediction
+            dCost_dPred = None
+            
+            # Cost: the cost function to calculate the prediction error margin
+            # check what cost function was passed to the parameter list
+            if cost_func == 'squared_error':
+                cost = NN_classification.squared_error(pred, output_data_labels[ri])
+                # Also calculate the derivative of the cost function with respect to prediction
+                dCost_dPred = 2 * (pred - output_data_labels[ri])
+            else:
+               print("Exception error - no cost function utilised, in training method", file=sys.stderr)
+               return
             
             # Derivative: bringing derivative from cost with respect to each of the network parameters (weights and bias)
-            dCost_dPred = 2 * (pred - target)
             if activation_func == 'sigmoid':
                 dPred_dWeightSum = NN_classification.sigmoid_derivation(self.training_weighted_sum)
+            elif activation_func == 'relu':
+                dPred_dWeightSum = NN_classification.relu_derivation(self.training_weighted_sum) 
             else:
-               dPred_dWeightSum = NN_classification.relu_derivation(self.training_weighted_sum) 
+                print("Exception error - no activation function utilised at derivation stage, in training method", file=sys.stderr)
+                return 
                 
             # Bias is just a number on its own added to the formula so its derivative is just 1
             dWeightSum_dB = 1
@@ -134,16 +167,39 @@ class NN_classification:
                 self.weights[i] = self.weights[i] - learning_rate * self.dCost_dWeights[i]
             self.bias = self.bias - learning_rate * dCost_dB
             
-            # for each 100th loop we're going to get a summary of the
+            # for each 50th loop we're going to get a summary of the
             # prediction compared to the actual ouput
             # to see if the prediction is as expected.
             # Anything in prediction above 0.5 should match value 
             # 1 of the actual ouptut. Any prediction below 0.5 should
             # match value of 0 for actual output 
-            if i % 100 == 0:
+            if i % 50 == 0:
                 self.costs.append(cost)
-               
-        plt.plot(self.costs)
+                    
+            # Compare prediction to target
+            error_margin = np.sqrt(np.square(pred - output_data_labels[ri]))
+            accuracy = (1 - error_margin) * 100
+            self.train_average_accuracy += accuracy
+            
+            # Evaluate whether guessed correctly or not based on classification binary problem 0 or 1 outcome. So if prediction is above 0.5 it guessed 1 and below 0.5 it guessed incorrectly. If it's dead on 0.5 it is incorrect for either guesses. Because it's no exactly a good guess for either 0 or 1. We need to set a good standard for the neural net model.
+            if (error_margin < 0.5) and (error_margin >= 0):
+                correct_pred += 1 
+            elif (error_margin >= 0.5) and (error_margin <= 1):
+                incorrect_pred += 1
+            else:
+                print("Exception error - 'margin error' for 'predict' method is out of range. Must be between 0 and 1, in training method", file=sys.stderr)
+                return
+        
+        # Calculate average accuracy from the predictions of all obervations in the training dataset
+        self.train_average_accuracy /= epochs
+        
+        # Print out results 
+        print('Average Accuracy: {}'.format(self.train_average_accuracy))
+        print('Correct predictions: {}, Incorrect Predictions: {}'.format(correct_pred, incorrect_pred))
+        print('costs = {}'.format(self.costs))
+        y = np.array(self.costs)
+        plt.plot(y)
+        plt.show()
 
         
     """
@@ -151,8 +207,11 @@ class NN_classification:
         Param:
             test_new_observations: (type: 2-dimensional array/list) The input test dataset. Please ensure this a 2-dimensional array/list (i.e. matrix).
     """
-    def predict(self, test_new_observations):
+    def predict(self, test_new_observations, test_new_target_outputs):
+        num_of_loops = int()
         self.test_new_observations = test_new_observations
+        correct_pred = int()
+        incorrect_pred = int()
         # loop through each observation in the matrix
         for observation in self.test_new_observations:
             # Loop through all the independent variables (x) in the observation
@@ -161,22 +220,52 @@ class NN_classification:
                 self.test_weighted_sum += observation[i] * self.weights[i]
             self.test_weighted_sum += self.bias
         
-        # Activation: 
-        if self.chosen_activation == 'sigmoid':
-           self.test_activation_func_output = NN_classification.sigmoid(self.training_weighted_sum)
-        else:
-            self.test_activation_func_output = NN_classification.relu(self.training_weighted_sum)
+            # Activation: 
+            if self.chosen_activation == 'sigmoid':
+                self.test_activation_func_output = NN_classification.sigmoid(self.test_weighted_sum)
+            elif self.chosen_activation == 'relu':
+                self.test_activation_func_output = NN_classification.relu(self.test_weighted_sum)
+            else:
+                print("Exception error - no activation function utilised, in predict method", file=sys.stderr)
+                return
+    
+            # Prediction: Because this is a single layer neural network, so the activation will be the same as the prediction
+            pred = self.test_activation_func_output
+            self.test_predictions.append(pred)
+            
+            # Compare prediction to target
+            error_margin = np.sqrt(np.square(pred - test_new_target_outputs[self.test_new_observations[observation]]))
+            accuracy = (1 - error_margin)* 100
+            self.test_average_accuracy += accuracy
+            num_of_loops +=1
+            
+            # Evaluate whether guessed correctly or not based on classification binary problem 0 or 1 outcome. So if prediction is above 0.5 it guessed 1 and below 0.5 it guessed incorrectly. If it's dead on 0.5 it is incorrect for either guesses. Because it's no exactly a good guess for either 0 or 1. We need to set a good standard for the neural net model.
+            if (error_margin < 0.5) and (error_margin >= 0):
+                correct_pred += 1 
+            elif (error_margin >= 0.5) and (error_margin <= 1):
+                incorrect_pred += 1
+            else:
+                print("Exception error - 'margin error' for 'predict' method is out of range. Must be between 0 and 1, in predict method", file=sys.stderr)
+                return
+                
+        # Calculate average accuracy from the predictions of all obervations in the test dataset
+        self.test_average_accuracy /= num_of_loops
         
-        # Prediction: Because this is a single layer neural network, so the activation will be the same as the prediction
-        pred = self.train_activation_func_output
-        return pred
+        # Print out results
+        for i in range(len(test_new_target_outputs)):
+            print('Prediction: {}, Target: {}'.format(self.test_predictions[i], test_new_target_outputs[i]))
+        print('Average Accuracy: {}'.format(self.test_average_accuracy))
+        print('Correct predictions: {}, Incorrect Predictions: {}'.format(correct_pred, incorrect_pred))
+        
+
         
 """
 The scenario:
 It all starts with a farmer. She like to measure everything around her. She was growing some flowers one day and realised she hadn’t measured them. 
 So she decided that this day was the perfect day to take out her rulers and take some measurements. 
 She has 2 types of flowers: red and blue flowers. She has many flowers. 
-She takes out her 2 rulers and lay one horizantally and the other vertically, connecting with the first ruler at a 90 degree angle, like an X and Y graph. 
+She takes ou
+t her 2 rulers and lay one horizantally and the other vertically, connecting with the first ruler at a 90 degree angle, like an X and Y graph. 
 She plucks 1 petal from the red flower and lays down on her rulers. 
 She measures the petal horizontally, along the petal's length, and then vertically along the petal’s width. 
 She repeats the same process with the blue flower. She goes on to measure numerous flowers of red and blue colour. 
@@ -202,3 +291,9 @@ data = array([[3,   1.5, 1],
 
 # separate data: split input, output, train and test data.
 X_train, y_train, X_test, y_test = data[:6, :-1], data[:6, -1], data[6:, :-1], data[6:, -1]
+
+nn_model = NN_classification()
+
+nn_model.simple_1_layer_classification_NN(X_train, y_train, 2, 500)
+
+nn_model.predict(X_test, y_test)
